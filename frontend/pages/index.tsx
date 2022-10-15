caf1ddd72d3dfa358ec6b2feb6f9e427591940d4
roshray <roshanroy759@gmail.com>
2022-10-15 22:54:39 +0530

    banner

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../components/Banner'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
 
       <Header/>
       {/* Banner */}
+      <Banner />
     </div>
   )
 }
