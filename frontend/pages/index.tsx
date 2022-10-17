78dc09384fec4c89a4d5bb25f2d703dab7893752
roshray <roshanroy759@gmail.com>
2022-10-17 23:17:42 +0530

    footer

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Banner from '../components/Banner'
+import Footer from '../components/Footer'
 import Header from '../components/Header'
 import LargeCard from '../components/LargeCard'
 import MediumCard from '../components/MediumCard'
@@ -58,6 +59,8 @@ export default function Home({ exploreData, cardsData}){
           buttonText="Get Inspired"
         />
       </main>
+
+      <Footer />
     </div>
   )
 }
