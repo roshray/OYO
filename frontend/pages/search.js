8af75c7b9a304a389c2d31d12fa90d89248c697b
roshray <roshanroy759@gmail.com>
2022-10-22 09:41:04 +0530

    router for search

---
+++ frontend/pages/search.js
@@ -0,0 +1,31 @@
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+function Search() {
+  return (
+    <div className="h-screen">
+        <Header />
+
+        <main className="flex">
+          <section className="flex-grow pt-14 px-6">
+            <p className="text-sm">300+ Stays for 5 number of guests</p>
+            <h1 className="text-3xl font-semibold mt-2 mb-6">Stays in Earth</h1>
+          
+            <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+              <p className="button">Cancellation Flexibility</p>
+              <p className="button">Type of Places</p>
+              <p className="button">Price</p>
+              <p className="button">Rooms and Beds</p>
+              <p className="button">More Filters</p>
+            </div>
+          
+          </section>
+          
+        </main> 
+
+        <Footer />
+    </div>
+  )
+}
+
+export default Search
