f098e9cc63cad0fee38748cf3f8f92b88c38be6a
roshray <roshanroy759@gmail.com>
2022-10-27 19:48:26 +0530

    loading

--- frontend/components/InfoCard.tsx
+++ frontend/components/InfoCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import { HeartIcon } from "@heroicons/react/outline"
 import { StarIcon} from "@heroicons/react/solid"
 
-function InfoCard({ img, location, title, description, star, price, total}) {
+function InfoCard({ img, location, title, description, star, price, total}: any) {
   return (
     <div className="flex py-7 px-2  pr-4 border-b cursor-pointer hover:opacity-80 hover:shadow-lg transition duration-200 ease-out first:border-t">
         <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
